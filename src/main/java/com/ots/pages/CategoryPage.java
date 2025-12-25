30ca8b99a91e3c1848195b0ceed836edcd1292cf
Om Gutty <om.gutty@gmail.com>
2025-12-25 15:23:26 +0530

    Created Category page with constructor

---
+++ src/main/java/com/ots/pages/CategoryPage.java
@@ -0,0 +1,15 @@
+package com.ots.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CategoryPage {
+
+WebDriver driver;
+	
+	public CategoryPage(WebDriver driver) 
+	{
+		this.driver=driver;
+	}
+	
+	
+}
