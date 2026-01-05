28cfbf2571716081dcd5fb1637325af29318a2c5
Om Gutty <om.gutty@gmail.com>
2026-01-05 14:52:06 +0530

    create constructor and initialize webdriver on category and course page

--- src/main/java/com/ots/pages/CoursePage.java
+++ src/main/java/com/ots/pages/CoursePage.java
@@ -1,5 +1,13 @@
 package com.ots.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class CoursePage {
+	
+	WebDriver driver;
+	
+	public  CoursePage(WebDriver driver) {
+		this.driver= driver;
+	}
 
 }
